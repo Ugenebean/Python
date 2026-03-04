b9dc30a633fe6f9a6e2e51ba774d9381d55ca5c6
hayyyyyydennnnnn <haydencoates8377@gmail.com>
2026-03-04 11:25:36 -0500

    2.6.1.4
    Remove the old commented hours-to-seconds example and replace it with an interactive script that reads a number from stdin, computes its square using float exponentiation, and prints the result. Variables `anything` and `something` are introduced to hold the input value and its square respectively.

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#this program computes the number of seconds in a given number of hours
-# this program has been written two days ago
-
-a = 2 # number of hours
-seconds = 3600 # number of seconds in 1 hour
-
-print("Hours: ", a) #printing the number of hours
-# print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
-
-#here we should also print "Goodbye", but a programmer didn't have time to write any code
-#this is the end of the program that computes the number of seconds in 3 hour
+anything = float(input("Enter a number: "))
+something = anything ** 2.0
+print(anything, "to the power of 2 is", something)
