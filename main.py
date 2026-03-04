890410a27a1ed5eb6ce4f629a816f57645cc1d07
hayyyyyydennnnnn <haydencoates8377@gmail.com>
2026-03-04 11:20:17 -0500

    2.5.1.2
    Remove numerous ad-hoc numeric debug prints and add a simple hours-to-seconds program scaffold. Introduces variables a = 2 and seconds = 3600, prints the hours value, and leaves the seconds-output line commented out. Adds explanatory comments and a TODO-style note about printing "Goodbye"; also contains a minor comment mismatch referencing 3 hours while a is 2.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-print("2")
-print(2)
-print(2+2)
-print(2 ** 3)
-print(2 ** 3.)
-print(2. ** 3)
-print(2. ** 3.)
-print(6 // 3)
-print(6 // 3.)
-print(6. // 3)
-print(6. // 3.)
-print(9 % 6 % 2)
-print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+#this program computes the number of seconds in a given number of hours
+# this program has been written two days ago
+
+a = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", a) #printing the number of hours
+# print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+#here we should also print "Goodbye", but a programmer didn't have time to write any code
+#this is the end of the program that computes the number of seconds in 3 hour
